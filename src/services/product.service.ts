f4ceaba1a20cd70ad964556a3ea68a9a835706d7
Yehor <kravtsovyehor@gmail.com>
2024-01-04 08:50:14 +0200

    Fixed getting products from db.json

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -3,7 +3,7 @@ import { IProduct } from "@declarations";
 import { $api } from "./axios.service";
 
 export class ProductService {
-  static getAll(params: any) {
+  static async getAll(params: any) {
     return $api.get<IProduct[]>("/products", { params }).then((response) => response.data);
   }
 }
