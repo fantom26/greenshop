af2c4c77ca8016e45fbafde28e8499772eb9fbaf
Yehor <kravtsovyehor@gmail.com>
2024-07-15 23:47:24 +0300

    Added helper for Queries component

---
+++ src/components/common/filter-list/helper.ts
@@ -0,0 +1,4 @@
+export const getSizeLetter = (name: string) => {
+  const matches = name.match(/\((\w+)\)/g);
+  return matches ? matches.map((match) => match.substring(1, match.length - 1)).join("") : "";
+};
