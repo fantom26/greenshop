36e710517594d11e01c12874d0809b2190b00c79
yehor <kravtsov.egor@kronas.com.ua>
2024-11-06 23:38:44 +0100

    Updated email validation for newsLetters

--- src/utils/schemas/newsletters.schema.ts
+++ src/utils/schemas/newsletters.schema.ts
@@ -1,12 +1,7 @@
 import { object, string } from "yup";
 
 export const NewslettersObject = object({
-  email: string()
-    .matches(
-      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g,
-      "Email must be a valid"
-    )
-    .required()
+  email: string().email("Email must be a valid").required()
 });
 
 export type NewslettersSchema = ReturnType<
