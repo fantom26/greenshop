b79df2314d73762b4de9b0b5daf91d43ac819e42
yehor <kravtsov.egor@kronas.com.ua>
2024-11-06 22:00:39 +0100

    Disabled desctucturing of env variables

--- src/shared/config/env-variables.ts
+++ src/shared/config/env-variables.ts
@@ -1,2 +1,3 @@
-export const {NEXT_PUBLIC_APP_URL} = process.env;
-export const {NEXT_PUBLIC_API_URL} = process.env;
+/* eslint-disable prefer-destructuring */
+export const NEXT_PUBLIC_APP_URL = process.env.NEXT_PUBLIC_APP_URL;
+export const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
