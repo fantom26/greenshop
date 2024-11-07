ca05e2726175197481e3211431f4e71cc6db49d4
yehor <kravtsov.egor@kronas.com.ua>
2024-11-07 09:32:18 +0100

    Replaced fetching products data via rtk query

--- src/pages/home/ui/products/components/main/index.tsx
+++ src/pages/home/ui/products/components/main/index.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 
-import { useProductsQuery } from "@/store/api";
+import { IProduct } from "@/utils/declarations";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
+import useSWR from "swr";
 
+import { NEXT_PUBLIC_API_URL } from "@/shared/config";
 import { Loader, Pagination, ProductCard, Typography } from "@/shared/ui";
 
 import { Queries } from "../sidebar/components";
@@ -12,30 +14,72 @@ import { MobileFilter } from "./components/mobile-filter";
 import { Sort } from "./components/sort";
 import * as S from "./main.styled";
 
+interface IProductsParams {
+  _limit: number;
+  _page: number;
+}
+
+const LIMIT = 9;
+const START_PAGE = 1;
+
+export const fetchProducts = async (params: Partial<IProductsParams>) => {
+  const query = new URLSearchParams(
+    params as Record<string, string>
+  ).toString();
+  const response = await fetch(`${NEXT_PUBLIC_API_URL}/products?${query}`);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch products");
+  }
+
+  const products: IProduct[] = (await response.json()) || [];
+  const links = response.headers.get("Link") || "";
+  const totalCount = Number(response.headers.get("X-Total-Count")) || 0;
+
+  return { products, meta: { links, totalCount } };
+};
+
 export function Main() {
   const { t } = useTranslation("home");
-
-  const LIMIT = 9;
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(START_PAGE);
 
   const { query } = useRouter();
 
-  const { data, isFetching } = useProductsQuery({
-    ...query,
-    _page: page,
-    _limit: LIMIT
-  });
+  const { data, isLoading } = useSWR(
+    [
+      "/products",
+      {
+        ...query,
+        _page: page,
+        _limit: LIMIT
+      }
+    ],
+    ([, params]) => fetchProducts(params)
+  );
 
   let content;
+  let pagination;
 
   if (data) {
     if (data.products.length > 0) {
       content = (
         <S.List>
-          {data?.products?.map((product) => (
+          {data.products.map((product) => (
             <ProductCard key={product._id} {...product} />
           ))}
         </S.List>
+      );
+
+      pagination = data.meta.links && (
+        <S.Bottom>
+          <Pagination
+            currentPage={page}
+            totalPages={Math.ceil(data.meta.totalCount / LIMIT)}
+            handleChangePage={(newPage: number) => {
+              setPage(newPage);
+            }}
+          />
+        </S.Bottom>
       );
     } else {
       content = (
@@ -44,7 +88,7 @@ export function Main() {
         </Typography>
       );
     }
-  } else if (isFetching) {
+  } else if (isLoading) {
     content = <Loader />;
   }
 
@@ -59,17 +103,7 @@ export function Main() {
         <Queries />
       </S.WrapperQueries>
       <S.Content>{content}</S.Content>
-      {data?.links && (
-        <S.Bottom>
-          <Pagination
-            currentPage={page}
-            totalPages={Math.ceil(data?.totalCount / LIMIT)}
-            handleChangePage={(page: number) => {
-              setPage(page);
-            }}
-          />
-        </S.Bottom>
-      )}
+      {pagination}
     </S.Main>
   );
 }
