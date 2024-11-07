22c05f3e76425c5a8461140baa8e447fed4dce22
yehor <kravtsov.egor@kronas.com.ua>
2024-11-07 10:20:03 +0100

    Got rid of redux on the project

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,8 +1,6 @@
 import { ReactNode } from "react";
 
-import { makeStore } from "@/store";
 import { ICartItem } from "@/utils/declarations";
-import { Provider as ReduxProvider } from "react-redux";
 
 import { CartProvider } from "@/app/providers/cart.provider";
 import { SWRProvider } from "@/app/providers/swr.provider";
@@ -16,9 +14,7 @@ export function Providers({
 }) {
   return (
     <SWRProvider>
-      <ReduxProvider store={makeStore()}>
-        <CartProvider defaultCart={defaultCart}>{children}</CartProvider>
-      </ReduxProvider>
+      <CartProvider defaultCart={defaultCart}>{children}</CartProvider>
     </SWRProvider>
   );
 }
--- src/store/api/fetch.tsx
+++
@@ -1,7 +0,0 @@
-import { fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
-
-import { NEXT_PUBLIC_API_URL } from "@/shared/config";
-
-export const baseQuery = fetchBaseQuery({
-  baseUrl: NEXT_PUBLIC_API_URL
-});
